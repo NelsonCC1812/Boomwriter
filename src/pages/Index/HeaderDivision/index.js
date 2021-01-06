a8a53e03aa4a875a6beb81333d728a8901210d30
nelson1812 <nelsoncc1812@gmail.com>
2021-01-06 03:18:55 +0000

    Header division completed

--- src/pages/Index/HeaderDivision/index.js
+++ src/pages/Index/HeaderDivision/index.js
@@ -6,11 +6,15 @@ import './header-division.css'
 
 const HeaderDivision = () => <section className='header-division'>
 
-    <h2>A global event that starts right in your classroom!</h2>
+    <div className='text-container'>
 
-    <p>Writing Bee Qualifying Rounds take place in individual classrooms around the world. Classroom-based winners and top vote-getters qualify for the Writing Bee Finals and a chance to become The Writing Bee Champion!</p>
+        <h2>A global event that starts right in your classroom!</h2>
 
-    <Link className='link btn' to='/#'> Start your classs writing bee </Link>
+        <p>Writing Bee Qualifying Rounds take place in individual classrooms around the world. Classroom-based winners and top vote-getters qualify for the Writing Bee Finals and a chance to become The Writing Bee Champion!</p>
+
+        <Link className='btn link' to='/#'> Start your classs writing bee </Link>
+
+    </div>
 
 </section>
 
