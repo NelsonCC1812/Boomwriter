68eb442bc1ebe83a75c83db56fbb0995e9790759
nelson1812 <nelsoncc1812@gmail.com>
2021-01-06 10:46:09 +0000

    Fixed some proportions to be more exactly with reference

---
+++ src/pages/Index/Timeline/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// data
+
+// styles
+import './timeline.css'
+
+const Timeline = () => <section className='timeline'>
+
+    <h2>The Writing Bee Timeline</h2>
+
+    <ul className='timeline-list'>
+
+    </ul>
+
+</section>
