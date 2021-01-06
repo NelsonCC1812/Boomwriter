37f2d95d37d6bec5ed16f7b61204e29304cb1821
nelson1812 <nelsoncc1812@gmail.com>
2021-01-06 05:24:19 +0000

    * structured index/testimony/experience
    * fixed on index/header-division p leter bigger and thiner

--- src/pages/Index/Testimony/Experience/index.js
+++ src/pages/Index/Testimony/Experience/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // styles
 import './experience.css'
 
 const Experience = () => <article className='experience'>
+
+    <img className='experience-img' src='./images/index/testimony/d-wilson.jpg' alt='D. Wilson' />
+
+    <div className='textContainer'>
+        <p className='text'>The Writing Bee was the most valuable writing experience I have had with my class in many years. My students were fully engaged - they collaborated, edited, imagined, improved, and happily worked for many hours on their chapter writing. My students improved their writing skills and also learned that they love writing. Invaluable lessons. Thank you, BoomWriter!</p>
+        <p className='sign'>D. Wilson</p>
+        <p className='ubication'>Camino Elementary School, Camino, California USA</p>
+    </div>
+
+    <Link to='#' className='link btn'>Start your class writing bee</Link>
 
 </article>
 
