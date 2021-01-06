56b01a164ff0fd6333e49a73ba7661fd8971b511
nelson1812 <nelsoncc1812@gmail.com>
2021-01-06 05:41:43 +0000

    Finished Index/Testimony/Experience

--- src/pages/Index/Testimony/Experience/index.js
+++ src/pages/Index/Testimony/Experience/index.js
@@ -8,9 +8,9 @@ const Experience = () => <article className='experience'>
 
     <img className='experience-img' src='./images/index/testimony/d-wilson.jpg' alt='D. Wilson' />
 
-    <div className='textContainer'>
+    <div className='text-container'>
         <p className='text'>The Writing Bee was the most valuable writing experience I have had with my class in many years. My students were fully engaged - they collaborated, edited, imagined, improved, and happily worked for many hours on their chapter writing. My students improved their writing skills and also learned that they love writing. Invaluable lessons. Thank you, BoomWriter!</p>
-        <p className='sign'>D. Wilson</p>
+        <p className='sign'>D.Wilson</p>
         <p className='ubication'>Camino Elementary School, Camino, California USA</p>
     </div>
 
